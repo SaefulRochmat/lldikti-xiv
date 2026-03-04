49e8f01f3543893db3bf9dfe151e017f2664eab1
Mochan Endeavour <mochanendeavour@gmail.com>
2026-03-04 20:27:49 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import GlobalModals from "@/components/sections/Modals/GlobalModals";
 import HeroCarouselSection from "@/components/sections/Carousel Image/HeroCarouselImage";
 import IntroductionSection from "@/components/sections/Sections Introduction/IntroductionSections";
 import NewsSection from "@/components/sections/NewsSection/NewsSection";
-import Footer from "@/components/sections/Footer/Footer";
+import Footer from "@/components/sections/Footer/Footer.js";
 export default function Home() {
   return (
     <div className="w-full h-screen">
