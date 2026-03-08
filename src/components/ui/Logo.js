d1ff2cd4ab66878b9dfbc94faadb555c598e141a
Mochan Endeavour <mochanendeavour@gmail.com>
2026-03-08 22:56:37 +0700

    update logo navbar

--- src/components/ui/Logo.js
+++ src/components/ui/Logo.js
@@ -1,22 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Logo () {
-    return (
-            <Link 
-                href="/"
-                className="flex items-center">
-                <Image
-                    src="/Logos/Logo.png"
-                    alt="Logo LLDIKTI XIV"
-                    width={75}
-                    height={75}
-                    className=""
-                /> 
-                <div className="ml-3">
-                    <h1 className="text-3xl font-bold text-[#153C91] tracking-wider">LLDIKTI</h1>
-                    <p className="text-sm text-[#153C91] tracking-wide">Wilayah XIV</p>
-                </div>
-            </Link>
-    );
+export default function Logo() {
+  return (
+    <Link href="/" className="flex items-center">
+      <Image
+        src="/Logos/Logo2.png"
+        alt="Logo LLDIKTI XIV"
+        width={200}
+        height={200}
+        className=""
+      />
+    </Link>
+  );
 }
