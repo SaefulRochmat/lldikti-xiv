b98fa72efc8b70071f685cdb468e18ec96cb9b76
MuhammadQuthbulAlam <mochanendeavour@gmail.com>
2026-08-14 08:55:56 +0700

    Update link terkait

--- src/data/links.js
+++ src/data/links.js
@@ -1,8 +1,8 @@
 export const tautanData = [
   {
     id: 1,
-    name: "Kemdikbudristek",
-    url: "https://www.kemdikbud.go.id",
+    name: "Kemdiktisaintek",
+    url: "https://kemdiktisaintek.go.id/",
     logo: "/Logos/kemendikbudristek.jpg",
   },
   {
@@ -14,7 +14,7 @@ export const tautanData = [
   {
     id: 3,
     name: "PDDikti",
-    url: "https://pddikti.kemdikbud.go.id",
+    url: "https://pddikti.kemdiktisaintek.go.id/",
     logo: "/Logos/pddikti.svg",
   },
   {
@@ -44,7 +44,7 @@ export const tautanData = [
   {
     id: 8,
     name: "PIN Ijazah",
-    url: "https://pin.kemdikbud.go.id",
+    url: "https://pisn.kemdiktisaintek.go.id/",
     logo: "/Logos/logo-tutwuri.png",
   },
   {
